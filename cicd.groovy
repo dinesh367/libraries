3d46eab2bcd7ee28d88466c7800e42da2d1dc563
dinesh367 <97458472+dinesh367@users.noreply.github.com>
2022-12-29 15:07:09 +0530

    Update cicd.groovy

--- cicd.groovy
+++ cicd.groovy
@@ -1,4 +1,4 @@
 def newDownload(repo)
 {
-  git credentialsId: '249d23ff-969c-4758-ac67-d4761a95541f', url: 'https://github.com/dinesh367/${repo}'
+  git credentialsId: 'Git_Hub Password', url: 'https://github.com/dinesh367/libraries/${repo}'
 }
