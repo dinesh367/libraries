8b6d084cd5da7970d0895f8878d38fcd81ac1492
dinesh367 <97458472+dinesh367@users.noreply.github.com>
2023-01-01 21:29:36 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,7 +2,18 @@ def newDownload(repo)
 {
   git "https://github.com/dinesh367/${repo}"
 }
+
 def newBuild()
 {
   sh 'mvn package'
+}
+
+def newDeploy(jobname,ip,appname)
+{
+  sh "scp -i temp_keypair.pem /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
+
+def runSelenium(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
